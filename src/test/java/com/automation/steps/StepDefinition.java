e5d41ae43c1b9fedb9d0cfb42c9fbce205426b7e
Tamerlan <tamerlash11@gmail.com>
2023-05-15 09:20:19 +0600

    Class 1

---
+++ src/test/java/com/automation/steps/StepDefinition.java
@@ -0,0 +1,43 @@
+package com.automation.steps;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefinition {
+    @Given("user is on login page")
+    public void user_is_on_login_page() {
+    }
+    @When("user enters valid credentials")
+    public void user_enters_valid_credentials() {
+    }
+    @Then("verify user is on home page")
+    public void verify_user_is_on_home_page() {
+    }
+    @When("user clicks on add to cart button of any random item")
+    public void user_clicks_on_add_to_cart_button_of_any_random_item() {
+    }
+    @When("click on cart icon")
+    public void click_on_cart_icon() {
+    }
+    @Then("verify user is on cart page")
+    public void verify_user_is_on_cart_page() {
+    }
+    @Then("verify item details on the cart page")
+    public void verify_item_details_on_the_cart_page() {
+    }
+    
+    @When("user click on continue shopping button")
+    public void user_click_on_continue_shopping_button() {
+    }
+
+    @When("user clicks on remove button of item")
+    public void userClicksOnRemoveButtonOfItem() {
+        
+    }
+
+    @Then("verify item is removed from the cart")
+    public void verifyItemIsRemovedFromTheCart() {
+    }
+}
